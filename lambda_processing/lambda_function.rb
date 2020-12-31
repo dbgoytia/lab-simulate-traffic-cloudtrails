ceefb09e10af369e4fa669cf2d52719ba15896a7
Diego Goytia <Diego_Canizales@epam.com>
2020-12-30 18:24:53 -0600

    Added the lambda code for processing the file into a secondary locatoin. Missing KMS encryption.

---
+++ lambda_processing/lambda_function.rb
@@ -0,0 +1,29 @@
+require 'json'
+require "aws-sdk-s3"
+
+def lambda_handler(event:, context:)
+    # Get the record
+    # event contains all information about uploaded object
+    puts("Event :", event)
+
+    # Bucket Name where file was uploaded
+    source_bucket_name = event['Records'][0]['s3']['bucket']['name']
+    
+    # Filename of object (with path)
+    file_key_name = event['Records'][0]['s3']['object']['key']
+
+    # shorten the name
+    short_name = file_key_name[0..20]
+
+    # Get reference to the file in S3
+    client = Aws::S3::Client.new(region: 'us-east-1')
+    s3 = Aws::S3::Resource.new(client: client)
+    object = s3.bucket(source_bucket_name).object(file_key_name)
+
+    # Move the file to a new bucket
+    object.move_to(bucket: 'thebeatles-lyrics-bucket-encrypted', key: short_name)
+    return {
+       'statusCode': 200,
+       'body': json.dumps('Hello from S3 events Lambda!')
+    }
+end
