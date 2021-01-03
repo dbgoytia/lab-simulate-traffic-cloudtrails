5efa6cfd7db5b3b7a715870a783c79a8a72742b8
Diego Goytia <Diego_Canizales@epam.com>
2021-01-03 16:06:37 -0600

    Added new ideas and useful commands. Awesome.

--- useful_commands.sh
+++ useful_commands.sh
@@ -14,3 +14,15 @@ aws lambda update-function-code --function-name move_to_encrypted --zip-file fil
 bundle install --path vendor/bundle
 zip -r function.zip function.rb vendor # Create a zip with vendor code available too.
 
+# Sample CloudWatch queries
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/CWL_QuerySyntax-examples.html
+
+# Maybe another solution is to use an external monitoring solution, like Datadog
+https://docs.datadoghq.com/getting_started/tagging/using_tags/?tab=assignment
+# This way, we can filter based on a tag
+
+# Maybe there's a way to modify the tags using a custom Lambda function.
+
+# Maybe there's also a way to create a custom dashboard in CloudWatch and filter events using the appropiate tag
+# The question here is: do you find the tags inside the Log Events?
+# Or do you only get to see tags inside CloudTrail events?
